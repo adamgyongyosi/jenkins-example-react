760499c223198a3d8921deb49a9d46dbd92e596f
Gyöngyösi Ádám <adam.gyongyosi@silinfo.hu>
2023-05-03 13:01:42 +0200

    qwewqewq

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. tesztelek 2222
+          Műxik?
         </p>
         <a
           className="App-link"
