c650355273a8cd090c9c51b27f751476a71acaf5
Gyöngyösi Ádám <adam.gyongyosi@silinfo.hu>
2023-05-03 13:20:25 +0200

    dfg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Műxik? auto?
+          Műxik? auto?ddd
         </p>
         <a
           className="App-link"
